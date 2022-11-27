5ccc68ce99c4ed57f26ea0167adfcac672eb2766
Enamulhaque0e <enamulhaq243@gmail.com>
2022-11-27 11:52:51 +0600

    all byer_all seller_ and myProduct delete api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000
@@ -48,6 +48,38 @@ app.post('/users', async (req, res) => {
   return res.send(result);
 });
 
+app.get('/buyers', async (req, res) => {
+  const buyer = req.query.buyer
+  
+  const query = {role : buyer}
+  const buyers = await usersCollection.find(query).toArray()
+  res.send(buyers);
+})
+app.delete('/buyer/:id',  async (req, res) => {
+  const id = req.params.id;
+  console.log(id)
+  const filter = { _id: ObjectId(id) };
+  const result = await usersCollection.deleteOne(filter);
+  res.send(result);
+})
+
+
+app.get('/sellers', async (req, res) => {
+  const seller = req.query.seller
+  
+  const query = {role : seller}
+  const sellers = await usersCollection.find(query).toArray()
+  res.send(sellers);
+})
+app.delete('/seller/:id',  async (req, res) => {
+  const id = req.params.id;
+  console.log(id)
+  const filter = { _id: ObjectId(id) };
+  const result = await usersCollection.deleteOne(filter);
+  res.send(result);
+})
+
+
 // usersGet
 app.get('/category', async (req, res) => {
   const query = {};
@@ -63,6 +95,25 @@ app.post('/product', async (req, res) => {
   const result = await productsCollection.insertOne(product);
   return res.send(result);
 });
+
+app.get('/products', async (req, res) => {
+  const email = req.query.email;
+  
+
+  const query = { email: email };
+  const products = await productsCollection.find(query).toArray();
+  res.send(products);
+});
+
+app.delete('/product/:id',  async (req, res) => {
+  const id = req.params.id;
+  console.log(id)
+  const filter = { _id: ObjectId(id) };
+  const result = await productsCollection.deleteOne(filter);
+  res.send(result);
+})
+
+
 
 //  category get
 app.get("/category/:id", async (req, res) => {
